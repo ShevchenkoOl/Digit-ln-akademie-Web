1cdc8055fa4cfca9ff7c81d3e01c4754e5db73ad
uzlabini@gmail.com <uzlabini@gmail.com>
2023-10-26 18:26:14 +0200

    created lekce 10

---
+++ JavaScript 1/lekce 10 Pole a zpracování dat/data.js
@@ -0,0 +1,32 @@
+const cisla = [7, 1, 4, 5, 2, 9, 3, 6, 8];
+const zvirata = [
+  "pes",
+  "kočka",
+  "králík",
+  "had",
+  "andulka",
+  "morče",
+  "krkavec",
+];
+const napoje = [
+  { nazev: "Pivo", cena: 12, skladem: true },
+  { nazev: "Rum", cena: 120, skladem: false },
+  { nazev: "Víno", cena: 85, skladem: true },
+  { nazev: "Whisky", cena: 450, skladem: true },
+  { nazev: "Vodka", cena: 280, skladem: false },
+  { nazev: "Becherovka", cena: 210, skladem: true },
+  { nazev: "Kofola", cena: 40, skladem: true },
+  { nazev: "Voda", cena: 15, skladem: false },
+];
+
+const shoppingList = [
+  'mouka',
+  'máslo',
+  'cukr',
+  'jablka',
+  'skořice',
+  'mléko',
+  'cibule',
+];
+
+export { cisla, zvirata, napoje, shoppingList };
